66e87ab017f0fc0d43889eae46f7a468e357d95f
Dumon <dmd.dumon@gmail.com>
2023-11-01 21:09:40 -0300

    First commit

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {Moment} from 'src/app/Moment';
+import {MomentService} from "src/app/services/moment.service";
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent {
+  btnText = 'Compartilhar!';
+
+  constructor(private momentService: MomentService) {
+  }
+
+  async createHandler(moment: Moment){
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if(moment.image) {
+      formData.append('image', moment.image)
+    }
+
+    //TODO
+    this.momentService.createMoment(formData).subscribe();
+    //exibir msg
+    //redirect
+  }
+}
